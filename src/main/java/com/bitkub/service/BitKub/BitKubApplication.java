d84da691846373fc60724cd4e5f57f5dc4751468
sammetas <raamaalayam@gmail.com>
2021-01-12 20:56:41 +0700

    Initial commit

---
+++ src/main/java/com/bitkub/service/BitKub/BitKubApplication.java
@@ -0,0 +1,13 @@
+package com.bitkub.service.BitKub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitKubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitKubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitkub/service/BitKub/BitKubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitkub.service.BitKub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitKubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
